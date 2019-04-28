68359013d23f76a79f60716ff989f738a842a93e
joshua-unseen <corpselight@gmail.com>
2019-04-27 21:38:51 -0700

    Functionify all the things.

--- liri.js
+++ liri.js
@@ -21,27 +21,8 @@ function Liri(command, parameter) {
             }
             var getURL = "https://rest.bandsintown.com/artists/" + parameter
                 + "/events?app_id=" + keys.axios.bit;
-            axios.get(getURL).then(function (ack) {
-                // console.log(ack.data);
-                var theConcerts = ack.data;
-                // Filter for venue, location, date for *each* event
-                if (theConcerts.length) {
-                    console.log("I found these " + parameter + " concerts:")
-                    for (var i = 0; i < theConcerts.length; i++) {
-                        theRegion = "";
-                        if (theConcerts[i].venue.region.length) {
-                            theRegion = theConcerts[i].venue.region + ", ";
-                        }
-                        console.log("");
-                        console.log(theConcerts[i].venue.name);
-                        console.log(theConcerts[i].venue.city + ", "
-                            + theRegion + theConcerts[i].venue.country);
-                        console.log(theConcerts[i].datetime);
-                    }
-                }
-                else {
-                    console.log("I'm sorry, " + parameter + " is not touring.")
-                }
+            axios.get(getURL).then(function (response) {
+                ConcertThis(response, parameter);
             }).catch(OhCrap);
             break;
         case "spotify-this-song":
@@ -51,18 +32,7 @@ function Liri(command, parameter) {
                 parameter = "ace of base the sign";
             }
             spotify.search({ type: "track", query: parameter, limit: 1 })
-                .then(function (ack) {
-                    // console.log(ack.tracks.items[0]);
-                    var data = ack.tracks.items[0];
-                    // Artist(s), name, preview link, album
-                    console.log("The song " + data.name + " is performed by ");
-                    data.artists.forEach(element => {
-                        console.log(element.name);
-                    });
-                    console.log("Here's a preview:");
-                    console.log(data.preview_url);
-                    console.log("The song is on the album " + data.album.name + ".");
-                }).catch(OhCrap);
+                .then(SpotifyThisSong).catch(OhCrap);
             break;
         case "movie-this":
             // do the OMDB thing with parameter (or "Mr Nobody")
@@ -71,16 +41,7 @@ function Liri(command, parameter) {
             }
             var getURL = "https://www.omdbapi.com/?t=" + parameter
                 + "&y=&plot=short&apikey=" + keys.axios.omdb;
-            axios.get(getURL).then(function (ack) {
-                // console.log(ack.data);
-                var theMovie = ack.data;
-                // Title, year, IMDB rating, RT rating, production country, language, plot, actors
-                console.log("The movie " + theMovie.Title + " was released in " + theMovie.Year + ".");
-                console.log("IMDB gives it " + theMovie.Ratings[0].Value+ ".");
-                console.log("Rotten Tomatoes gives it " + theMovie.Ratings[1].Value+ ".");
-                console.log("It was filmed in " + theMovie.Language + ", in " + theMovie.Country + ", with " + theMovie.Actors+ ".");
-                console.log(theMovie.Plot);
-            }).catch(OhCrap);
+            axios.get(getURL).then(MovieThis).catch(OhCrap);
             break;
         case "do-what-it-says":
             // use fs to read random.txt and execute as if the contents were passed as argv
@@ -92,14 +53,57 @@ function Liri(command, parameter) {
             break;
     }
 }
+function ConcertThis(ack, band) {
+    var theConcerts = ack.data;
+    // console.log(theConcerts);
+    // Filter for venue, location, date for *each* event
+    if (theConcerts.length) {
+        console.log("I found these " + band + " concerts:")
+        for (var i = 0; i < theConcerts.length; i++) {
+            theRegion = "";
+            if (theConcerts[i].venue.region.length) {
+                theRegion = theConcerts[i].venue.region + ", ";
+            }
+            console.log("");
+            console.log(theConcerts[i].venue.name);
+            console.log(theConcerts[i].venue.city + ", "
+                + theRegion + theConcerts[i].venue.country);
+            console.log(theConcerts[i].datetime);
+        }
+    }
+    else {
+        console.log("I'm sorry, " + band + " is not touring.")
+    }
+}
+function SpotifyThisSong(ack) {
+    // console.log(ack.tracks.items[0]);
+    var data = ack.tracks.items[0];
+    // Artist(s), name, preview link, album
+    console.log("The song " + data.name + " is performed by ");
+    data.artists.forEach(element => {
+        console.log(element.name);
+    });
+    console.log("Here's a preview:");
+    console.log(data.preview_url);
+    console.log("The song is on the album " + data.album.name + ".");
+}
+function MovieThis(ack) {
+    var theMovie = ack.data;
+    // Title, year, IMDB rating, RT rating, production country, language, plot, actors
+    console.log("The movie " + theMovie.Title + " was released in " + theMovie.Year + ".");
+    console.log("IMDB gives it " + theMovie.Ratings[0].Value + ".");
+    console.log("Rotten Tomatoes gives it " + theMovie.Ratings[1].Value + ".");
+    console.log("It was filmed in " + theMovie.Language + ", in " + theMovie.Country + ", with " + theMovie.Actors + ".");
+    console.log(theMovie.Plot);
+}
 function DoWhatItSays(err, data) {
     if (err) {
         OhCrap(err);
     }
     else {
-        console.log(data);
+        // console.log(data);
         readArgs = data.split(",");
-        console.log(readArgs);
+        // console.log(readArgs);
         Liri(readArgs[0], readArgs[1]);
     }
 }
